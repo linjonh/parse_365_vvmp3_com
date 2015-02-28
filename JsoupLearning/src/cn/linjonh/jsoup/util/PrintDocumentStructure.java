b427f2959e8ddb5dc69b3a1410d2ba8d35b3c94f
john.lin <john.lin@tpv-tech.com>
2015-02-28 09:37:44 +0800

    print document

--- JsoupLearning/src/cn/linjonh/jsoup/util/PrintDocumentStructure.java
+++ JsoupLearning/src/cn/linjonh/jsoup/util/PrintDocumentStructure.java
@@ -2,7 +2,8 @@ package cn.linjonh.jsoup.util;
 
 
 public class PrintDocumentStructure {
-
+	static final String web_1="http://tu.meinvdd.com/meinv/5977.html";
+	static final String web_2="http://www.meizitu.com";
 	public PrintDocumentStructure() {
 		// TODO Auto-generated constructor stub
 	}
@@ -12,7 +13,7 @@ public class PrintDocumentStructure {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(ConnUtil.getHtmlDocument("http://tu.meinvdd.com/meinv/5977.html"));
+		System.out.println(ConnUtil.getHtmlDocument(web_2));
 	}
 
 }
