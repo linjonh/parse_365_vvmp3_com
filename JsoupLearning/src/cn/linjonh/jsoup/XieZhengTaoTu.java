19c553abdcbea7f018214aa5784db4f8990231fd
john.lin <john.lin@tpv-tech.com>
2015-02-28 13:22:37 +0800

    comment and change saveing file path

--- JsoupLearning/src/cn/linjonh/jsoup/XieZhengTaoTu.java
+++ JsoupLearning/src/cn/linjonh/jsoup/XieZhengTaoTu.java
@@ -16,25 +16,21 @@ import cn.linjonh.jsoup.util.Utils;
 
 public class XieZhengTaoTu {
 
-	public XieZhengTaoTu() {
-		// TODO Auto-generated constructor stub
-	}
-
 	static final String mUrl = "http://77.bbs560.com/xz/cn/";
 	static final String mip = "http://198.44.250.182/xz/cn/";
-	static final String filePathDir = "C:/XieZhenTaoTu";
+	static final String filePathDir = "E:/XieZhenTaoTu";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("start");
-		// final Document doc = ConnUtil.getHtmlDocument(mUrl);
-		File in = new File(filePathDir + "/xz.html");
+//		 final Document doc = ConnUtil.getHtmlDocument(mUrl);
+		File in = new File("E:/xz.html");
 		try {
 			final Document doc = Jsoup.parse(in, "gb2312");
 			Elements alinks = doc.select("a");
-			System.out.println(alinks);
+			// System.out.println(alinks);
 			download(alinks);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -45,14 +41,15 @@ public class XieZhengTaoTu {
 	/**
 	 * 
 	 * @param alinks
+	 *            parse html to get all image album links
 	 * @return
 	 */
 	private static List<String> getAlinks(Elements alinks) {
 		List<String> girdItemLinks = new ArrayList<String>();
 		for (Element el : alinks) {
-			System.out.println("getAlinks: " + el.attr("href"));
 			girdItemLinks.add(el.attr("href"));
-			print("baseUri:" + el.baseUri());
+			// System.out.println("getAlinks: " + el.attr("href"));
+			// print("baseUri:" + el.baseUri());
 		}
 		return girdItemLinks;
 	}
@@ -73,6 +70,11 @@ public class XieZhengTaoTu {
 		return girdItemImgLinks;
 	}
 
+	/**
+	 * 
+	 * @param alinks
+	 *            all album links
+	 */
 	private static void download(Elements alinks) {
 		List<String> alinklist = getAlinks(alinks);
 		int count = 1;
@@ -91,13 +93,18 @@ public class XieZhengTaoTu {
 
 	}
 
+	/**
+	 * 
+	 * @param url
+	 *            detail album url
+	 */
 	private static void downloadDetailAlbum(String url) {
 		Document document = ConnUtil.getHtmlDocument(url);
 		Elements elements = document.select("option");
 		for (Element element : elements) {
-//			print("baseUri: " + element.baseUri());
+			// print("baseUri: " + element.baseUri());
 			String link = element.baseUri() + element.attr("value");
-			print("downloadDetailAlbum:"+link);
+			// print("downloadDetailAlbum:"+link);
 			DonwloadUtil.DonwloadImg(link, filePathDir);
 		}
 	}
