167431a37af4b2751f068f1d309b4f138af6b19b
john.lin <john.lin@tpv-tech.com>
2015-03-20 17:20:29 +0800

    add handle exception

--- JsoupLearning/src/cn/linjonh/jsoup/util/DonwloadUtil.java
+++ JsoupLearning/src/cn/linjonh/jsoup/util/DonwloadUtil.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 import java.io.FileOutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import cn.linjonh.jsoup.util.Utils;
@@ -65,6 +66,7 @@ public class DonwloadUtil {
 	 * @param fileName
 	 * @return
 	 */
+	@SuppressWarnings("finally")
 	public static boolean donwloadImg(String imgFileUrl, String dirPath1, String fileName) {
 		boolean flag = false;
 		File imageFile = null;
@@ -105,11 +107,28 @@ public class DonwloadUtil {
 		DataInputStream in = null;
 		HttpURLConnection connection = null;
 		try {
-
 			URL url = new URL(imgFileUrl);
-			connection = (HttpURLConnection) url.openConnection();
-			in = new DataInputStream(connection.getInputStream());
-
+			while (in == null) {
+				try {
+					connection = (HttpURLConnection) url.openConnection();
+					in = new DataInputStream(connection.getInputStream());
+				} catch (Exception e) {
+					Utils.print("connection error:" + e.toString() + "\n^^^^^^^imgFileUrl:" + imgFileUrl);
+					Utils.writeLog(dir.getAbsolutePath(), "connection error:" + e.toString() + "\n^^^^^^^imgFileUrl:"
+							+ imgFileUrl);
+					if (e instanceof FileNotFoundException) {
+						break;
+					}
+					if (e instanceof MalformedURLException) {
+						break;
+					}
+					if (e instanceof NullPointerException) {
+						break;
+					}
+				}
+				Utils.print("connect again.........................imgFileUrl" + imgFileUrl);
+				Utils.writeLog(dir.getAbsolutePath(), "connect again.....................imgFileUrl" + imgFileUrl);
+			}
 			out = new DataOutputStream(new FileOutputStream(imageFile));
 
 			byte[] buffer = new byte[4096];
@@ -117,9 +136,15 @@ public class DonwloadUtil {
 			while ((count = in.read(buffer)) > 0) {
 				out.write(buffer, 0, count);
 			}
-			out.close();
-			in.close();
-			connection.disconnect();
+			if (out != null) {
+				out.close();
+			}
+			if (in != null) {
+				in.close();
+			}
+			if (connection != null) {
+				connection.disconnect();
+			}
 			String log = "save File: " + imageFile.getAbsolutePath() + " URL: " + imgFileUrl;
 			Utils.print(log);
 			Utils.writeLog(dir.getAbsolutePath(), log);
@@ -139,16 +164,25 @@ public class DonwloadUtil {
 					out.close();
 			} catch (final IOException e) {
 				e.printStackTrace();
+				Utils.writeLog(dir.getAbsolutePath(), e.toString());
 			}
 			try {
 				if (in != null)
 					in.close();
 			} catch (final IOException e) {
 				e.printStackTrace();
+				Utils.writeLog(dir.getAbsolutePath(), e.toString());
 			}
-			connection.disconnect();
+			try {
+				if (connection != null) {
+					connection.disconnect();
+				}
+			} catch (Exception e2) {
+				Utils.writeLog(dir.getAbsolutePath(), e2.toString());
+				e2.printStackTrace();
+			}
 			return flag;
 		}
 	}
-	
+
 }
